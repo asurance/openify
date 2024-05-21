dc1b848150fff3ac86dda60d503a4d3f3258a466
谢宇超 <784672616@qq.com>
2024-05-22 02:45:56 +0800

    修复rspress插件类型报错

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,8 @@
 import { moduleTools, defineConfig } from '@modern-js/module-tools';
 import { modulePluginDoc } from '@modern-js/plugin-rspress';
 
+export type { UserConfigExport } from '@modern-js/core';
+
 export default defineConfig({
   plugins: [
     moduleTools(),

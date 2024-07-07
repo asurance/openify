6ca85e5373d071cdbb68c9e8abd3bb504e08e9ce
谢宇超 <784672616@qq.com>
2024-07-07 23:19:19 +0800

    补充"抛错示例"

---
+++ demos/error.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Button, Modal } from '@douyinfe/semi-ui';
+import { OpenableProps, openify } from 'openify';
+
+type MyModalProps = OpenableProps<void>;
+
+const MyModal = ({ visible, onClose, afterClose }: MyModalProps) => {
+  const [showError, setShowError] = useState(false);
+  if (showError) {
+    throw new Error('error');
+  }
+  return (
+    <Modal
+      visible={visible}
+      onOk={onClose}
+      onCancel={onClose}
+      afterClose={afterClose}
+    >
+      <Button
+        onClick={() => {
+          setShowError(true);
+        }}
+      >
+        error
+      </Button>
+    </Modal>
+  );
+};
+
+const openModal = openify<void, MyModalProps, MyModalProps>(MyModal);
+
+export default () => (
+  <Button onClick={() => openModal({ title: '自定义弹窗' })}>自定义弹窗</Button>
+);
